d302341ffd30b14215acfce6f7a44c3ea60a9102
thekarampuri <akhilkarampuri25@gmail.com>
2026-06-07 23:07:34 +0530

    Deleted unnecessary files

--- cleanup.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const dir = 'C:\\Color Reduction';
-if (fs.existsSync(dir)) {
-  fs.rmSync(dir, { recursive: true, force: true });
-  console.log('Cleaned up C:\\Color Reduction');
-} else {
-  console.log('Already clean');
-}
